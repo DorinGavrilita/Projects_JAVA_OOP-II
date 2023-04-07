9d4f9e22d886c7f12aabd0cd3b671e5e19c8e529
DorinGavrilita <gavrilitadorin5@gmail.com>
2023-04-08 00:49:15 +0300

    CollectionsUsage

--- src/collections/CollectionsUsage.java
+++ src/collections/CollectionsUsage.java
@@ -1,98 +1,101 @@
 package collections;
 
 import java.io.*;
+import java.text.*;
 import java.time.*;
-import java.time.format.*;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class CollectionsUsage {
+
     public static void main(String[] args) {
-        // Read clients from input file and store in a list
-        List<String> clients = readClientsFromFile();
+        String inputFileName = "D:\\Dorin\\USARB\\POO II\\Java\\Project_1\\src\\resources\\client-input.txt";
+        String outputFileName = "D:\\Dorin\\USARB\\POO II\\Java\\Project_1\\src\\resources\\client-final.txt";
+        listOfClientNames(inputFileName);
+        addClientsAge(inputFileName, outputFileName);
+    }
 
-        // Sort clients in alphabetical order
-        sortClients(clients);
-
-        // Extract distinct first names of employees
-        List<String> firstNames = extractFirstNames(clients);
-
-        // Output the sorted list of clients and distinct first names to console
-        System.out.println("Sorted List of Clients: ");
+    private static void listOfClientNames(String fileName) {
+        ArrayList<String> clients = readClientsFromFile(fileName);
+        Collections.sort(clients);
+        System.out.println("Clients sorted in alphabetical order:");
         for (String client : clients) {
             System.out.println(client);
         }
-
-        System.out.println("\nDistinct First Names of Employees: ");
+        System.out.println();
+        ArrayList<String> firstNames = extractFirstNames(clients);
+        System.out.println("Distinct first names of employees:");
         for (String firstName : firstNames) {
             System.out.println(firstName);
         }
-
-        // Append date of birth and age to input file and store in a new file
-        appendDateOfBirthAndAge(clients, "src/resources/client-input.txt", "src/resources/client-final.txt");
     }
 
-    private static List<String> readClientsFromFile() {
-        List<String> clients = new ArrayList<>();
-
-        try (BufferedReader br = new BufferedReader(new FileReader("src/resources/client-input.txt"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                clients.add(line);
-            }
-        } catch (IOException e) {
-            System.err.println("Error reading clients from file: " + e.getMessage());
+    private static void addClientsAge(String inputFileName, String outputFileName) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> originalLines = readClientsFromFile(inputFileName);
+        Collections.sort(originalLines);
+        ArrayList<String> modifiedLines = new ArrayList<>();
+        System.out.print("Enter the date of birth (dd/MM/yyyy) for:");
+        for (String line : originalLines) {
+            String[] name = line.split(" ");
+            System.out.print("\n\t" + name[0] + " " + name[1] + " ");
+            String dateString = scanner.nextLine();
+            LocalDate birthDate = LocalDate.parse(dateString, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            Period age = Period.between(birthDate, LocalDate.now());
+            String modifiedLine = line + "|" + birthDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + "|" + age.getYears();
+            modifiedLines.add(modifiedLine);
         }
+        writeClientData(modifiedLines, outputFileName);
+    }
 
+
+    private static ArrayList<String> readClientsFromFile(String fileName) {
+        ArrayList<String> clients = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
+            while (line != null) {
+                clients.add(line);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (Exception e) {
+            System.err.format("Exception occurred trying to read '%s'.", fileName);
+            e.printStackTrace();
+        }
         return clients;
     }
 
-    private static void sortClients(List<String> clients) {
-        clients.sort(Comparator.comparing(c -> c.split("\\|")[0]));
-    }
-
-    private static List<String> extractFirstNames(List<String> clients) {
-        List<String> firstNames = new ArrayList<>();
-        Set<String> distinctFirstNames = new HashSet<>();
-
+    private static ArrayList<String> extractFirstNames(ArrayList<String> clients) {
+        /*ArrayList<String> firstNames = new ArrayList<>();
         for (String client : clients) {
-            String[] clientDetails = client.split("\\|");
-            String firstName = clientDetails[0];
-            String lastName = clientDetails[1];
-
-            if (lastName.equals("employee") && !distinctFirstNames.contains(firstName)) {
+            String[] tokens = client.split(" ");
+            String firstName = tokens[0];
+            if (!firstNames.contains(firstName)) {
                 firstNames.add(firstName);
-                distinctFirstNames.add(firstName);
             }
+        }*/
+        // Alternatively, use a HashSet to store distinct first names:
+        HashSet<String> firstNamesSet = new HashSet<>();
+        for (String client : clients) {
+            String[] tokens = client.split(" ");
+            firstNamesSet.add(tokens[0]);
         }
-
+        ArrayList<String> firstNames = new ArrayList<>(firstNamesSet);
+        Collections.sort(firstNames);
         return firstNames;
     }
 
-    private static void appendDateOfBirthAndAge(List<String> clients, String inputFilename, String outputFilename) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFilename))) {
-            BufferedReader br = new BufferedReader(new FileReader(inputFilename));
-
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] clientDetails = line.split("\\|");
-                String firstName = clientDetails[0];
-                String lastName = clientDetails[1];
-                String dobString = clientDetails[2];
-
-                LocalDate dob = LocalDate.parse(dobString, formatter);
-                int age = Period.between(dob, LocalDate.now()).getYears();
-
-                String newLine = line + "|" + formatter.format(dob) + "|" + age;
-
-                bw.write(newLine);
-                bw.newLine();
+    private static void writeClientData(ArrayList<String> clients, String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (String client : clients) {
+                writer.write(client);
+                writer.newLine();
             }
-
-            br.close();
         } catch (IOException e) {
-            System.err.println("Error appending date of birth and age to file: " + e.getMessage());
+            e.printStackTrace();
         }
     }
+
+
 }
---
+++ src/oop/Student.java
@@ -0,0 +1,10 @@
+package oop;
+
+import java.time.LocalDate;
+
+public class Student {
+    private String name;
+    private LocalDate birthDate;
+    private double avg;
+    private String hobby;
+}
